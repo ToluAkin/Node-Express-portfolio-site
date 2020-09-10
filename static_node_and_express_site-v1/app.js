846d8eec09fd00e843006d76d92c6146966c9947
Toluwalase <tolu@cottacush.com>
2020-09-10 16:52:41 +0100

    Added more images completed error logging added error pages changed font-family app starts on npm start

--- static_node_and_express_site-v1/app.js
+++ static_node_and_express_site-v1/app.js
@@ -22,7 +22,12 @@ app.get('/', (req, res) => {
 app.get('/project/:id', (req, res) => {
     const projectId = req.params.id;
     const project = projects.find( ({ id }) => id === +projectId );
-    res.render('project', { project })
+
+    if (project) {
+        res.render('project', { project });
+    } else {
+        res.sendStatus(404);
+    }
 })
 
 //render about page
@@ -30,6 +35,28 @@ app.get('/about', (req, res) => {
     res.render('about');
 });
 
+//Handling errors
+app.use((req, res, next) => {
+    const err = new Error();
+    err.status = 404;
+    err.message = `The page requested does not exist. You can navigate from the homepage.`
+    console.log(`Error: ${err.status}. ${err.message}`);
+    next(err);
+    res.render('page-not-found');
+});
+
+app.use((err, req, res, next) => {
+    if (err) {
+        console.log('Global error handler called', err)
+    }
+
+    if (err.status === 404) {
+        res.status(404).render('page-not-found', { err });
+    } else {
+        err.message = err.message || `Ooops! It looks like something went wrong on the server.`
+        res.status(err.status || 500).render('error', { err });
+    }
+});
 //listening on port 3000
 app.listen(3000, () => {
     console.log("This app is running on http://localhost:3000/");
