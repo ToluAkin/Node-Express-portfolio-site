8abed9d9eed4e4bbf8dc7ad9073a12db3e87a83d
Toluwalase <tolu@cottacush.com>
2020-09-13 13:40:53 +0100

    updated application

--- app.js
+++ app.js
@@ -44,7 +44,9 @@ app.use((err, req, res, next) => {
     }
 });
 
-//listening on port 3000
-app.listen(3000, () => {
-    console.log("This app is running on http://localhost:3000/");
+
+//listening on port 5000
+let port = process.env.PORT || 5000
+app.listen(port, () => {
+    console.log("This app is running on http://localhost:5000/");
 });
